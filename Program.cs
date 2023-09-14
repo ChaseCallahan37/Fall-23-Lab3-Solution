d76711683a65d786ae89eabd742dc1679ec6cce1
Chase Callahan <ctcallahan2@crimson.ua.edu>
2023-09-14 09:31:27 -0500

    Finished project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// ***** Main ****** //
+
+string season = GetSeason();
+
+string message = GetSeasonMessage(season);
+
+System.Console.WriteLine(message);
+
+// ****** End *******//
+
+static string GetSeason(){
+    System.Console.WriteLine("What is the current season? (winter, spring, summer, fall)");
+    return Console.ReadLine();
+}
+
+static string GetSeasonMessage(string season){
+    string message;
+    if(season == "fall"){
+        message = FavoriteSeasonMessage("sherlenthia");
+    } else if(season == "winter"){
+        message = FavoriteSeasonMessage("manav");
+    } else{
+        message = "It is not a TAs favorite season";
+    }
+
+    return message;
+}
+
+static string FavoriteSeasonMessage(string name){
+    return "Yay, it is " + name + "'s favorite season 🥳";
+}
